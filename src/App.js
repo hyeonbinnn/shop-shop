c9061923ac1ee65ba6dd172abed4dac31ccbef60
hyeonbin jeong <duafuuu@gmail.com>
2023-09-23 03:59:15 +0900

    add: 글로벌 스타일 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import Router from './routes/Router';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/Theme';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Router />
     </ThemeProvider>
   );
