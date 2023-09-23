b525908f3061be9f5afeb6f25a61f923f8c89199
hyeonbin jeong <duafuuu@gmail.com>
2023-09-24 04:09:33 +0900

    add: 스크롤탑 버튼 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Router from './routes/Router';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/Theme';
 import GlobalStyle from './styles/GlobalStyle';
+import ScrollTopButton from './components/common/Button/ScrollTopButton';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router />
+      <ScrollTopButton />
     </ThemeProvider>
   );
 }
---
+++ src/components/common/Button/ScrollTopButton.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import scroll from '../../../assets/icon-scroll.svg';
+
+const ScrollTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisible = () => {
+    if (window.scrollY > 500) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  window.addEventListener('scroll', toggleVisible);
+
+  return (
+    <Button onClick={scrollTop} show={isVisible}>
+      <img src={scroll} alt="스크롤 위로 올리는 화살표 아이콘" />
+    </Button>
+  );
+};
+
+export default ScrollTopButton;
+
+const Button = styled.button`
+  display: ${({ show }) => (show ? 'block' : 'none')};
+  position: fixed;
+  bottom: 80px;
+  right: 50px;
+  z-index: 10;
+  border-radius: 50%;
+
+  img {
+    width: 55px;
+    height: 55px;
+  }
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    bottom: 60px;
+    right: 30px;
+
+    img {
+      width: 45px;
+      height: 45px;
+    }
+  }
+`;
