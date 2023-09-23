256205440b818c865ce9b6752f983777e8b0884f
hyeonbin jeong <duafuuu@gmail.com>
2023-09-23 20:57:21 +0900

    fix: 파일 위치 이동

--- src/components/Product/ProductData.jsx
+++ src/components/Product/ProductData.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ProductList from '../components/Product/ProductList';
+import ProductList from './ProductList';
 
 const ProductData = () => {
   const [data, setData] = useState([]);
