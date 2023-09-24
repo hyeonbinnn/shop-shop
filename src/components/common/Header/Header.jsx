02f2019415942dfe5e3a4be3fbbfb539ab5338aa
hyeonbin jeong <duafuuu@gmail.com>
2023-09-25 00:45:39 +0900

    add: 장바구니, 유저 버튼 추가

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import logo from '../../../assets/icon-logo.svg';
+import cart from '../../../assets/icon-cart.svg';
+import user from '../../../assets/icon-user.svg';
 
 const Header = () => {
   return (
     <LogoHeader>
-      <Link to="/">
-        <LinkImg src={logo} alt="쇼핑몰 로고" />
-      </Link>
+      <LogoBox>
+        <Link to="/">{/* <LinkImg src={logo} alt="쇼핑몰 로고" /> */}</Link>
+      </LogoBox>
+      <TopBar>
+        <BarList>
+          <Link>
+            <ListBox>
+              <img src={cart} alt="장바구니 아이콘" />
+              <span>장바구니</span>
+            </ListBox>
+          </Link>
+        </BarList>
+        <BarList>
+          <Link>
+            <ListBox>
+              <img src={user} alt="로그인 아이콘" />
+              <span>로그인</span>
+            </ListBox>
+          </Link>
+        </BarList>
+      </TopBar>
     </LogoHeader>
   );
 };
@@ -16,17 +35,44 @@ const Header = () => {
 export default Header;
 
 const LogoHeader = styled.header`
-  width: 650px;
-  height: 300px;
-  margin: 40px auto;
+  display: flex;
+`;
+
+// 로고
+const LogoBox = styled.div`
+  width: 300px;
 
   @media ${(props) => props.theme.mediaQuery.mobile} {
-    max-width: 375px;
-    height: 200px;
   }
 `;
 
 const LinkImg = styled.img`
   width: 100%;
   height: 100%;
+`;
+
+// 장바구니, 로그인
+const TopBar = styled.ul`
+  display: flex;
+  gap: 20px;
+`;
+
+const BarList = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+const ListBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 30px;
+    margin-bottom: 5px;
+  }
+
+  span {
+    font-weight: bold;
+  }
 `;
