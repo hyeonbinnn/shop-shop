1f6dfd5a082c52b6201f9da4523e4745edef2b1e
hyeonbin jeong <duafuuu@gmail.com>
2023-10-03 13:09:15 +0900

    fix: 상품 목록 관련 파일 코드 병합

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import Product from './Product';
-import * as S from './Product.style';
+import React, { useState, useEffect } from 'react';
+import * as S from './ProductList.style';
+import { getAllProduct } from './../../api/product';
 
-const ProductList = ({ data }) => {
+const ProductList = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getAllProduct()
+      .then((response) => {
+        setData(response);
+      })
+      .catch((error) => {
+        console.error('데이터 불러오는 중 에러 발생', error);
+      });
+  }, []);
+
   return (
     <S.Product>
       <S.ProductList>
         {data.map((item) => (
-          <Product key={item.id} product={item} />
+          <S.ProductItem key={item.id}>
+            <S.ProductBox>
+              <S.ProductImg src={item.image} alt={item.product_name} />
+            </S.ProductBox>
+            <S.ProductName className="product-name sl-ellipsis">{item.product_name}</S.ProductName>
+            <S.LikeBtn className="like-btn"></S.LikeBtn>
+            <S.ProductPrice>
+              <strong>
+                {item.price}
+                <span>원</span>
+              </strong>
+            </S.ProductPrice>
+          </S.ProductItem>
         ))}
       </S.ProductList>
     </S.Product>
---
+++ src/pages/HomePage/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from '../../components/common/Footer/Footer';
+import Header from '../../components/common/Header/Header';
+import ProductList from '../../components/Product/ProductList';
+import Slider from '../../components/Slider/Slider';
+
+const Home = ({ data }) => {
+  return (
+    <>
+      <Header />
+      <Slider />
+      <ProductList />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
