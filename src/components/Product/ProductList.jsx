9cd4ace676393bd403b0c29ebef5abba124cb173
hyeonbin jeong <duafuuu@gmail.com>
2023-10-03 15:44:20 +0900

    style: 페이지 로딩 처리 구현

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -1,38 +1,49 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import * as S from './ProductList.style';
 import { getAllProduct } from './../../api/product';
+import Loading from '../common/Loading/Loading';
 
 const ProductList = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getAllProduct()
       .then((response) => {
         setData(response);
+        setLoading(false);
       })
       .catch((error) => {
         console.error('데이터 불러오는 중 에러 발생', error);
+        setLoading(false);
       });
   }, []);
 
   return (
     <S.Product>
+      {loading && <Loading />}
       <S.ProductList>
-        {data.map((item) => (
-          <S.ProductItem key={item.id}>
-            <S.ProductBox>
-              <S.ProductImg src={item.image} alt={item.product_name} />
-            </S.ProductBox>
-            <S.ProductName className="product-name sl-ellipsis">{item.product_name}</S.ProductName>
-            <S.LikeBtn className="like-btn"></S.LikeBtn>
-            <S.ProductPrice>
-              <strong>
-                {item.price}
-                <span>원</span>
-              </strong>
-            </S.ProductPrice>
-          </S.ProductItem>
-        ))}
+        {data &&
+          data.map((item) => (
+            <Link to={'/productDetail/:product_id'} key={item.id}>
+              <S.ProductItem>
+                <S.ProductBox>
+                  <S.ProductImg src={item.image} alt={item.product_name} />
+                </S.ProductBox>
+                <S.ProductName className="product-name sl-ellipsis">
+                  {item.product_name}
+                </S.ProductName>
+                <S.LikeBtn className="like-btn"></S.LikeBtn>
+                <S.ProductPrice>
+                  <strong>
+                    {item.price}
+                    <span>원</span>
+                  </strong>
+                </S.ProductPrice>
+              </S.ProductItem>
+            </Link>
+          ))}
       </S.ProductList>
     </S.Product>
   );
