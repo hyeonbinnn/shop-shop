913e75734186c23efb98f2c70bb6f933200abdf8
hyeonbin jeong <duafuuu@gmail.com>
2023-10-03 20:00:57 +0900

    fix: 페이지 추가 및 컴포넌트 생성

--- src/pages/ProductPage/ProductDetail.jsx
+++ src/pages/ProductPage/ProductDetail.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ProductDetail = () => {
-  return <div>ProductDetail</div>;
+  return <>ProductDetail</>;
 };
 
 export default ProductDetail;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Store from './../pages/StorePage/Store';
 import ProductDetail from './../pages/ProductPage/ProductDetail';
 import Cart from './../pages/CartPage/Cart';
+import Home from '../pages/HomePage/Home';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Store />} />
-        <Route path="/product" element={<ProductDetail />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Home />} />
+        <Route path="/join" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/payment" element={<Cart />} />
+        <Route path="/productDetail/:product_id" element={<ProductDetail />} />
+        <Route path="*" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   );
