bf1c7d2c99a2ad03bedfa2fa36301ee8cc5f4b01
hyeonbin jeong <duafuuu@gmail.com>
2023-10-15 22:44:10 +0900

    add: 커밍순 페이지 구현

--- src/pages/ErrorPage/ComingSoon.jsx
+++ src/pages/ErrorPage/ComingSoon.jsx
@@ -1,7 +1,160 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+import { Link, useNavigate } from 'react-router-dom';
+import comingSoonBg from '../../assets/images/comingSoonBg.svg';
+import comingSoonLogo from '../../assets/images/comingSoonLogo.svg';
+import next from '../../assets/icons/icon-next-white.svg';
 
 const ComingSoon = () => {
-  return <div>ComingSoon</div>;
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+  return (
+    <Container>
+      <Wrap>
+        <LogoBox>
+          <img src={comingSoonLogo} alt="커밍순 로고 이미지" />
+          <span>
+            기능을 업데이트중입니다. <br />
+            빠른 시일 내에 찾아뵙도록 하겠습니다. <br />
+            감사합니다 🖤
+          </span>
+        </LogoBox>
+        <ButtonBox>
+          <PrevBtn onClick={goBack}>
+            <img src={next} alt="이전으로 가는 화살표 아이콘" />
+            <span>이전 페이지</span>
+          </PrevBtn>
+          <Link to="/">
+            <NextBtn>
+              <span>메인 페이지</span>
+              <img src={next} alt="다음으로 가는 화살표 아이콘" />
+            </NextBtn>
+          </Link>
+        </ButtonBox>
+      </Wrap>
+    </Container>
+  );
 };
 
 export default ComingSoon;
+
+export const commonButton = ({ theme }) => `
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  background-color:${theme.colors.black};
+  padding: 15px 20px;
+  border-radius: 15px;
+`;
+
+export const commonText = ({ theme }) => `
+  font-size: 16px;
+  font-weight: bold;
+  color:${theme.colors.white};
+`;
+
+export const commonImage = `
+  width: 18px;
+  height: 18px;
+`;
+
+export const commonMediaQuery = css`
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    padding: 12px 14px;
+
+    span {
+      font-size: 14px;
+    }
+    img {
+      width: 15px;
+      height: 15px;
+    }
+  }
+`;
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: url(${comingSoonBg}) center/cover no-repeat;
+  background-attachment: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  gap: 60px;
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    gap: 40px;
+  }
+`;
+
+const LogoBox = styled.div`
+  width: 800px;
+  display: flex;
+  flex-direction: column;
+
+  img {
+    width: 100%;
+  }
+
+  span {
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 27px;
+  }
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    width: 430px;
+
+    span {
+      font-size: 17px;
+    }
+  }
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+  padding: 0px 20px;
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    gap: 30px;
+  }
+`;
+
+const PrevBtn = styled.button`
+  ${commonButton}
+  span {
+    ${commonText}
+  }
+  img {
+    ${commonImage}
+    transform: rotate(180deg);
+  }
+  ${commonMediaQuery}
+`;
+
+const NextBtn = styled.button`
+  ${commonButton}
+  span {
+    ${commonText}
+  }
+  img {
+    ${commonImage}
+  }
+  ${commonMediaQuery}
+`;
---
+++ src/styles/CommonStyle.jsx
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+// LoginForm, SignUpForm 공통 스타일
+export const CommonContainer = styled.section`
+  padding: 30px 20px;
+  text-algin: center;
+  background-color: ${({ theme }) => theme.colors.white};
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    padding: 30px 0px;
+  }
+`;
+
+export const CommonHeader = styled.header`
+  width: 350px;
+  margin: 30px auto;
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    width: 300px;
+  }
+`;
+
+export const CommonMain = styled.main`
+  width: 600px;
+  margin: 0 auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    width: 400px;
+  }
+`;
+
+export const CommonTabList = styled.li`
+  display: inline-block;
+  width: 50%;
+  padding: 20px;
+  border: ${({ theme }) => `1px solid ${theme.colors.gray}`};
+  border-bottom: none;
+  border-radius: 10px 10px 0 0;
+  font-size: 17px;
+  font-weight: bold;
+  line-height: 22px;
+  text-align: center;
+  box-sizing: border-box;
+  background-color: ${({ theme, isSelected }) =>
+    isSelected === true ? theme.colors.white : theme.colors.lightGray};
+  cursor: pointer;
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    padding: 15px;
+    font-size: 15px;
+  }
+`;
+
+export const CommonForm = styled.form`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
+  padding: 40px;
+  border: ${({ theme }) => `1px solid ${theme.colors.gray}`};
+  border-top: none;
+  border-radius: 0 0 10px 10px;
+  box-sizing: border-box;
+
+  p {
+    font-size: 13px;
+  }
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    p {
+      margin-top: -9px;
+    }
+  }
+`;
