9595b962f8ea1462b6d2780cf1c4d36fecae0f22
hyeonbin jeong <duafuuu@gmail.com>
2023-10-17 02:26:31 +0900

    add: 사용자 인증별 메뉴 구현

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,48 +1,111 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getSearchProduct } from '../../../api/product';
+import { getCookie } from '../../../services/cookies';
 import * as S from './Header.style';
-import cart from '../../../assets/icons/icon-cart.svg';
-import user from '../../../assets/icons/icon-user.svg';
 import logo from '../../../assets/icons/icon-logo.svg';
-import search from '../../../assets/icons/icon-search.svg';
+import DropDown from './../../DropDown/DropDown';
 
 const Header = () => {
+  const token = getCookie('token');
+  const loginType = getCookie('loginType');
+
+  const navigate = useNavigate();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchText, setSearchText] = useState('');
+
+  const toggleMenu = () => {
+    setIsOpen(true);
+  };
+
+  const handleSearch = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const onClickSearch = async () => {
+    const searchResults = await getSearchProduct(searchText);
+    navigate('/searchResult', {
+      state: searchResults,
+    });
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      onClickSearch();
+    }
+  };
+
   return (
-    <S.HeaderWrap>
-      <S.Container>
-        <S.LogoSearchBox>
-          <S.LogoBox>
-            <Link to="/">
-              <S.LogoImg src={logo} alt="쇼핑몰 로고 아이콘" />
-            </Link>
-          </S.LogoBox>
-          <S.SearchBox>
-            <input type="text" placeholder="상품을 검색해주세요" />
-            <img src={search} alt="검색 아이콘" />
-          </S.SearchBox>
-        </S.LogoSearchBox>
-        <S.HeaderNav>
-          <S.HeaderMenuList>
+    <S.Container>
+      <S.LogoSearchBox>
+        <S.LogoBox>
+          <Link to="/">
+            <S.LogoImg src={logo} alt="쇼핑몰 로고 아이콘" />
+          </Link>
+        </S.LogoBox>
+        <S.SearchBox>
+          <label htmlFor="search">
+            <input
+              id="search"
+              type="text"
+              placeholder="상품을 검색해 보세요!"
+              onChange={handleSearch}
+              onKeyDown={handleKeyDown}
+            />
+            <button onClick={onClickSearch}>
+              <span className="a11y-hidden">검색 버튼</span>
+            </button>
+          </label>
+        </S.SearchBox>
+      </S.LogoSearchBox>
+
+      {loginType === 'SELLER' ? (
+        <S.MenuContainer>
+          <S.MenuBox modal={isOpen} width="56">
+            <S.UserBtn onClick={toggleMenu} modal={isOpen} />
+            <span>마이페이지</span>
+            {isOpen && <DropDown />}
+          </S.MenuBox>
+          <Link to="/sellerCenter">
+            <S.SellerBtn>판매자 센터</S.SellerBtn>
+          </Link>
+        </S.MenuContainer>
+      ) : (
+        <S.MenuContainer>
+          {token ? (
             <Link to="/cart">
-              <S.List>
-                <S.ListBox>
-                  <img src={cart} alt="장바구니 아이콘" />
-                  <span>장바구니</span>
-                </S.ListBox>
-              </S.List>
+              <S.MenuBox>
+                <S.CartBtn />
+                <span>장바구니</span>
+              </S.MenuBox>
             </Link>
-            <Link to="/Login">
-              <S.List>
-                <S.ListBox>
-                  <img src={user} alt="로그인 아이콘" />
-                  <span>로그인</span>
-                </S.ListBox>
-              </S.List>
+          ) : (
+            <Link to="/login">
+              <S.MenuBox>
+                <S.CartBtn />
+                <span>장바구니</span>
+              </S.MenuBox>
             </Link>
-          </S.HeaderMenuList>
-        </S.HeaderNav>
-      </S.Container>
-    </S.HeaderWrap>
+          )}
+
+          {token ? (
+            <S.MenuBox modal={isOpen} width="56">
+              <S.UserBtn onClick={toggleMenu} modal={isOpen} />
+              <span>마이페이지</span>
+              {isOpen && <DropDown />}
+            </S.MenuBox>
+          ) : (
+            <Link to="/login">
+              <S.MenuBox width="56">
+                <S.UserBtn />
+                <span>로그인</span>
+              </S.MenuBox>
+            </Link>
+          )}
+        </S.MenuContainer>
+      )}
+    </S.Container>
   );
 };
 
