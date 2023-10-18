5e53a1dac1c19968a1c75f8211e398bd3121836e
hyeonbin jeong <duafuuu@gmail.com>
2023-10-18 17:32:02 +0900

    fix: 상태 관리 및 라우팅 처리 방법 변경

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -1,48 +1,54 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import * as S from './ProductList.style';
 import { getAllProduct } from './../../api/product';
+import { setProducts } from '../../redux/slices/slices';
 import Loading from '../common/Loading/Loading';
 
 const ProductList = () => {
-  const [data, setData] = useState([]);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getAllProduct()
       .then((response) => {
-        setData(response);
+        dispatch(setProducts(response));
         setLoading(false);
       })
       .catch((error) => {
         console.error('데이터 불러오는 중 에러 발생', error);
         setLoading(false);
       });
-  }, []);
+  }, [dispatch]);
+
+  const products = useSelector((state) => state.products.products);
 
   return (
     <S.Product>
       {loading && <Loading />}
       <S.ProductList>
-        {data &&
-          data.map((item) => (
-            <Link to={'/productDetail/:product_id'} key={item.id}>
-              <S.ProductItem>
-                <S.ProductBox>
-                  <S.ProductImg src={item.image} alt={item.product_name} />
-                </S.ProductBox>
-                <S.ProductName className="product-name sl-ellipsis">
-                  {item.product_name}
-                </S.ProductName>
-                <S.LikeBtn className="like-btn"></S.LikeBtn>
-                <S.ProductPrice>
-                  <strong>
-                    {item.price}
-                    <span>원</span>
-                  </strong>
-                </S.ProductPrice>
-              </S.ProductItem>
-            </Link>
+        {products &&
+          products.map((item) => (
+            <S.ProductItem
+              key={item.product_id}
+              onClick={() => navigate(`/productDetail/${item.product_id}`)}
+            >
+              <S.ProductBox>
+                <S.ProductImg src={item.image} alt={item.product_name} />
+              </S.ProductBox>
+              <S.ProductName className="product-name sl-ellipsis">
+                {item.product_name}
+              </S.ProductName>
+              <S.LikeBtn className="like-btn"></S.LikeBtn>
+              <S.ProductPrice>
+                <strong>
+                  {item.price}
+                  <span>원</span>
+                </strong>
+              </S.ProductPrice>
+            </S.ProductItem>
           ))}
       </S.ProductList>
     </S.Product>
