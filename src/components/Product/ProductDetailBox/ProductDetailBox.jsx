114f3f83f8c198ffe53d5d8ebef95f462e8b8a12
hyeonbin jeong <duafuuu@gmail.com>
2023-10-21 20:03:08 +0900

    style: Product 디테일 박스 틀 잡기

---
+++ src/components/Product/ProductDetailBox/ProductDetailBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ProductDetailBox = () => {
+  return (
+    <>
+      <h1 className="a11y-hidden">상품 상세 페이지</h1>
+      <div>
+        <img />
+        <span></span>
+        <strong></strong>
+        <div>
+          <strong>
+            <span></span>
+          </strong>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductDetailBox;
---
+++ src/components/Product/ProductDetailBox/ProductDetailBox.style.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ProductImg = styled.img``;
+
+export const ProductStore = styled.span``;
+
+export const ProductName = styled.strong``;
+
+export const ProductPrice = styled.div``;
