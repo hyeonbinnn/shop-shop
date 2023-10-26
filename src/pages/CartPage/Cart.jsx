6998e92515ab53234f926af0131f1defa62a2cf7
hyeonbin jeong <duafuuu@gmail.com>
2023-10-26 21:36:06 +0900

    style: 컴포넌트 렌더링

--- src/pages/CartPage/Cart.jsx
+++ src/pages/CartPage/Cart.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './../../components/common/Header/Header';
+import CartItem from './../../components/CartBox/CartItem/CartItem';
 
 const Cart = () => {
   return (
     <>
       <Header />
+      <CartItem />
     </>
   );
 };
