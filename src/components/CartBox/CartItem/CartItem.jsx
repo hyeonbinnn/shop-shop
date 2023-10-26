447161d0e0672e77e67cb95dd048be6010162e8a
hyeonbin jeong <duafuuu@gmail.com>
2023-10-26 21:34:41 +0900

    add: 장바구니 아이템 및 총가격 컴포넌트 생성

---
+++ src/components/CartBox/CartItem/CartItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CartItem = ({ product_id, cart_item_id, quantity, is_active }) => {
+  return <></>;
+};
+
+export default CartItem;
---
+++ src/components/CartBox/TotalPrice/TotalPrice.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TotalPrice = () => {
+  return <div>TotalPrice</div>;
+};
+
+export default TotalPrice;
