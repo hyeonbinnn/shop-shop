00081de6e78b40741714cec33f52033b46d229ae
hyeonbin jeong <duafuuu@gmail.com>
2023-10-28 02:49:58 +0900

    add: 장바구니 헤더 구현

--- src/components/CartBox/CartItem/CartItemHeader.jsx
+++ src/components/CartBox/CartItem/CartItemHeader.jsx
@@ -1,7 +1,74 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const CartItemHeader = () => {
-  return <div>CartItemHeader</div>;
+  return (
+    <Container>
+      <input id="allCheckBox" type="checkbox" />
+      <span>상품정보</span>
+      <span>수량</span>
+      <span>상품금액</span>
+    </Container>
+  );
 };
 
 export default CartItemHeader;
+
+const Container = styled.div`
+  max-width: 1280px;
+  display: flex;
+  justify-content: space-between;
+  padding: 18px 130px 18px 40px;
+  margin: 30px auto;
+  background-color: ${({ theme }) => theme.colors.lightGray};
+  border-radius: 10px;
+
+  span {
+    font-size: 17px;
+    font-weight: bold;
+  }
+
+  input[type='checkbox'] {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    border: ${({ theme }) => `2px solid ${theme.colors.secondary}`};
+    background-color: ${({ theme }) => theme.colors.white};
+    appearance: none;
+
+    &:checked {
+      ::after {
+        content: '';
+        position: absolute;
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        border: ${({ theme }) => `2px solid ${theme.colors.white}`};
+        background-color: ${({ theme }) => theme.colors.secondary};
+        appearance: none;
+        box-sizing: border-box;
+      }
+    }
+  }
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    padding: 17px 50px 17px 30px;
+
+    span {
+      font-size: 16px;
+      line-height: 16px;
+    }
+
+    input[type='checkbox'] {
+      width: 18px;
+      height: 18px;
+
+      &:checked {
+        ::after {
+          width: 14px;
+          height: 14px;
+        }
+      }
+    }
+  }
+`;
