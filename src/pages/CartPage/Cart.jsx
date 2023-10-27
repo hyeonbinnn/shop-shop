1acc6f1d7dcc981c8db0d844a030a8ff4adf544e
hyeonbin jeong <duafuuu@gmail.com>
2023-10-28 02:55:20 +0900

    add: 장바구니 아이템 연결 및 체크박스, 총가격 기능 구현

--- src/pages/CartPage/Cart.jsx
+++ src/pages/CartPage/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import * as S from './Cart.style';
 
 import { getCookie } from '../../services/cookies';
 import { getCartList } from '../../api/cart';
@@ -13,9 +14,10 @@ import CartItem from './../../components/CartBox/CartItem/CartItem';
 import CartItemHeader from './../../components/CartBox/CartItem/CartItemHeader';
 import TotalPrice from './../../components/CartBox/TotalPrice/TotalPrice';
 import Loading from '../../components/common/Loading/Loading';
+import CartItemEmpty from './../../components/CartBox/CartItem/CartItemEmpty';
 
 const Cart = () => {
-  const naivgate = useNavigate();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const token = getCookie('token');
 
@@ -24,16 +26,112 @@ const Cart = () => {
 
   let cartArr = [];
 
+  const carts = useSelector((state) => state.carts.carts);
+
+  useEffect(() => {
+    getCartList(token).then((res) => {
+      dispatch(setCarts(res.results));
+      setLoading(false);
+    });
+    setLoading(true);
+  }, [dispatch, token]);
+
+  const getData = () => {
+    const result = Promise.all(
+      carts.map((el) => {
+        return getDetailProduct(el.product_id);
+      })
+    );
+    return result;
+  };
+
+  const checkedItemHandler = (checked, item, id) => {
+    if (item.stock !== 0 && checked) {
+      setCheckedArr((prev) => [...prev, item]);
+    } else {
+      setCheckedArr(checkedArr.filter((i) => i.product_id !== id));
+    }
+
+    const allCheckBox = document.getElementById('allCheckBox');
+    if (allCheckBox.checked && !checked) {
+      allCheckBox.checked = false;
+    } else {
+      allCheckBox.checked = true;
+    }
+  };
+
+  const checkedAllHandler = (checked) => {
+    if (checked) {
+      getData().then((arr) => {
+        setCheckedArr(arr.filter((i) => i.stock !== 0));
+      });
+
+      console.log(checkedArr);
+    } else {
+      setCheckedArr([]);
+    }
+  };
+
+  checkedArr.map((p) =>
+    carts
+      .filter((c) => p.product_id === c.product_id)
+      .map((c) => {
+        p.quantity = c.quantity;
+        p.is_active = c.is_active;
+        return cartArr.push(p);
+      })
+  );
+
+  const priceArr = cartArr.filter((i) => i.is_active).map((i) => i.price * i.quantity);
+
+  const feeArr = cartArr.filter((i) => i.is_active).map((i) => i.shipping_fee);
+
+  const total = (arr) => {
+    const res = arr.reduce((acc, cur) => {
+      acc += cur;
+      return acc;
+    }, 0);
+    return res;
+  };
+
+  const totalPrice = total(priceArr);
+  const totalFee = total(feeArr);
+  const totalPay = totalPrice + totalFee;
+
+  const goToPayment = () => {
+    const selectedItems = cartArr.map((selected) => ({
+      ...selected,
+      order_kind: 'cart_order',
+    }));
+    navigate('/payment', {
+      state: { products: selectedItems },
+    });
+  };
+
   return (
     <>
       {loading && <Loading />}
       <Header />
-      <section>
+      <S.CartSection>
         <h1>장바구니</h1>
-        <CartItemHeader />
-        <CartItem />
-        <TotalPrice />
-      </section>
+        <CartItemHeader checkedAllHandler={checkedAllHandler} />
+        {carts.length > 0 ? (
+          carts.map((item) => (
+            <CartItem
+              key={item.product_id}
+              checkedItemHandler={checkedItemHandler}
+              checkedArr={checkedArr}
+              {...item}
+            />
+          ))
+        ) : (
+          <CartItemEmpty />
+        )}
+        <TotalPrice totalPrice={totalPrice} totalFee={totalFee} totalPay={totalPay} />
+        <button onClick={goToPayment} disabled={checkedArr.length === 0}>
+          주문하기
+        </button>
+      </S.CartSection>
       <Footer />
     </>
   );
