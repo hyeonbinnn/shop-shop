9880aaf162e4cf45e94bb9d5176e914d33334985
hyeonbin jeong <duafuuu@gmail.com>
2023-10-29 12:10:00 +0900

    add: 장바구니 총 가격 기능 구현

--- src/components/CartBox/TotalPrice/TotalPrice.jsx
+++ src/components/CartBox/TotalPrice/TotalPrice.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import * as S from './TotalPrice.style';
+import minusIcon from '../../../assets/icons/icon-minus-line.svg';
+import plusIcon from '../../../assets/icons/icon-plus-line.svg';
 
-const TotalPrice = () => {
-  return <div>TotalPrice</div>;
+const TotalPrice = ({ totalPrice, totalFee, totalPay }) => {
+  return (
+    <S.Container>
+      <S.PriceBox>
+        <span>총 상품 금액</span>
+        <strong>
+          {totalPrice.toLocaleString()}
+          <span>원</span>
+        </strong>
+      </S.PriceBox>
+      <img src={minusIcon} alt="빼기 아이콘" />
+      <S.PriceBox>
+        <span>상품 할인</span>
+        <strong>
+          0<span>원</span>
+        </strong>
+      </S.PriceBox>
+      <img src={plusIcon} alt="더하기 아이콘" />
+      <S.PriceBox>
+        <span>배송비</span>
+        <strong>
+          {totalFee.toLocaleString()}
+          <span>원</span>
+        </strong>
+      </S.PriceBox>
+      <S.TotalBox>
+        <span>결제 예정 금액</span>
+        <strong>
+          {totalPay.toLocaleString()} <span>원</span>
+        </strong>
+      </S.TotalBox>
+    </S.Container>
+  );
 };
 
 export default TotalPrice;
--- src/components/CartBox/TotalPrice/TotalPrice.style.jsx
+++ src/components/CartBox/TotalPrice/TotalPrice.style.jsx
@@ -1,1 +1,46 @@
 import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1280px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.lightGray};
+  padding: 50px 100px;
+  margin: 30px auto;
+  border-radius: 10px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+
+    span {
+      font-size: 17px;
+      font-weight: bold;
+    }
+  }
+
+  img {
+    width: 34px;
+    height: 34px;
+    background-color: ${({ theme }) => theme.colors.white};
+    border-radius: 100%;
+    padding: 8px;
+  }
+`;
+
+export const PriceBox = styled.div`
+  strong {
+    font-size: 22px;
+  }
+`;
+
+export const TotalBox = styled.div`
+  margin-left: 120px;
+
+  strong {
+    font-size: 35px;
+    color: ${({ theme }) => theme.colors.error};
+  }
+`;
