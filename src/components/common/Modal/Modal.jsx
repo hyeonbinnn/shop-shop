24a9b3e1e39678e99f3267680cf3dc0a2a36d778
hyeonbin jeong <duafuuu@gmail.com>
2023-10-31 23:17:03 +0900

    feat: 사용자 관련 로그인, 장바구니, 수량, 삭제 모달 구현

--- src/components/common/Modal/Modal.jsx
+++ src/components/common/Modal/Modal.jsx
@@ -1,7 +1,103 @@
 import React from 'react';
+import * as S from './Modal.style';
+import { Link } from 'react-router-dom';
 
-const Modal = () => {
-  return <div>Modal</div>;
+import { getCookie } from '../../../services/cookies';
+import { putEditQuantityCart, deleteCartItem } from '../../../api/cart';
+import { deleteProduct } from '../../../api/product';
+
+import QuantityButton from '../../ProductDetail/QuantityButton/QuantityButton';
+
+const Modal = ({
+  option,
+  productId,
+  cartId,
+  active,
+  orderNum,
+  minusStock,
+  plusStock,
+  openModal,
+  closeModal,
+  loginType,
+}) => {
+  const token = getCookie('token');
+
+  const editBtn = async () => {
+    putEditQuantityCart(token, cartId, productId, orderNum, active);
+  };
+
+  const deleteBtn = async () => {
+    if (cartId) {
+      deleteCartItem(token, cartId);
+    } else if (productId) {
+      deleteProduct(token, productId);
+    }
+  };
+
+  const handleModalClick = (e) => {
+    e.stopPropagation();
+  };
+
+  const preventOutsideClick = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <S.ModalBg onClick={preventOutsideClick}>
+      {openModal && (
+        <S.Container onClick={handleModalClick}>
+          {option === 'quantity' ? (
+            <S.Custom>
+              <QuantityButton
+                orderNum={orderNum}
+                minusStock={minusStock}
+                plusStock={plusStock}
+                loginType={loginType}
+              />
+            </S.Custom>
+          ) : null}
+
+          {option === 'delete' ? (
+            <S.Question>상품을 삭제하시겠습니까?</S.Question>
+          ) : option === 'login' ? (
+            <S.Question>
+              로그인이 필요한 서비스입니다.
+              <br /> 로그인 하시겠습니까?
+            </S.Question>
+          ) : option === 'cart' ? (
+            <S.Question>
+              이미 장바구니에 있는 상품입니다.
+              <br /> 장바구니로 이동하시겠습니까?
+            </S.Question>
+          ) : null}
+
+          <S.AnswerBox>
+            {option === 'delete' || option === 'quantity' ? (
+              <S.FirstBtn onClick={closeModal}>취소</S.FirstBtn>
+            ) : option === 'login' ? (
+              <S.FirstBtn onClick={closeModal}>아니오</S.FirstBtn>
+            ) : option === 'cart' ? (
+              <S.FirstBtn onClick={closeModal}>아니오</S.FirstBtn>
+            ) : null}
+            {option === 'delete' ? (
+              <S.SecondeBtn onClick={deleteBtn}>확인</S.SecondeBtn>
+            ) : option === 'quantity' ? (
+              <S.SecondeBtn onClick={editBtn}>수정</S.SecondeBtn>
+            ) : option === 'login' ? (
+              <Link to="/login">
+                <S.SecondeBtn>예</S.SecondeBtn>
+              </Link>
+            ) : option === 'cart' ? (
+              <Link to="/cart">
+                <S.SecondeBtn>예</S.SecondeBtn>
+              </Link>
+            ) : null}
+          </S.AnswerBox>
+          <S.DeleteBtn onClick={closeModal} />
+        </S.Container>
+      )}
+    </S.ModalBg>
+  );
 };
 
 export default Modal;
--- src/components/common/Modal/Modal.style.jsx
+++ src/components/common/Modal/Modal.style.jsx
@@ -1,1 +1,71 @@
 import styled from 'styled-components';
+import { DeleteButton } from '../../CartBox/CartItem/CartItem.style';
+
+export const ModalBg = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  cursor: not-allowed;
+  z-index: 19;
+`;
+
+export const Container = styled.div`
+  width: 360px;
+  padding: 60px 0px 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  gap: 35px;
+  transform: translate(-50%, -50%);
+  background-color: ${({ theme }) => theme.colors.white};
+  border: ${({ theme }) => `1px solid ${theme.colors.gray}`};
+  border-radius: 5px;
+  z-index: 20;
+  cursor: pointer;
+`;
+
+export const Question = styled.p`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const AnswerBox = styled.div`
+  display: flex;
+  gap: 10px;
+
+  button {
+    width: 100px;
+    padding: 10px 0;
+    border-radius: 5px;
+    font-size: 15px;
+    font-weight: bold;
+    border: ${({ theme }) => `1px solid ${theme.colors.gray}`};
+  }
+`;
+
+export const FirstBtn = styled.button`
+  background-color: ${({ theme }) => theme.colors.white};
+  color: ${({ theme }) => theme.colors.lightGrayText};
+`;
+
+export const SecondeBtn = styled.button`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const DeleteBtn = styled(DeleteButton)`
+  top: 15px;
+  right: 15px;
+`;
+
+export const Custom = styled.div`
+  margin-top: -20px;
+  height: 80px;
+`;
