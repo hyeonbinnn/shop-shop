36ad124366be24c73700bd3b9e3cb30aa62f4990
hyeonbin jeong <duafuuu@gmail.com>
2023-11-02 19:36:56 +0900

    add: 빈장바구니 구현

--- src/components/CartBox/CartItem/CartItemEmpty.jsx
+++ src/components/CartBox/CartItem/CartItemEmpty.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const CartItemEmpty = () => {
-  return <div>CartItemEmpty</div>;
+  return (
+    <Container>
+      <strong>장바구니에 담긴 상품이 없습니다.</strong>
+      <p>원하는 상품을 장바구니에 담아 보세요!</p>
+    </Container>
+  );
 };
 
 export default CartItemEmpty;
+
+const Container = styled.div`
+  padding: 150px 0px;
+
+  strong {
+    font-size: 18px;
+  }
+
+  p {
+    font-size: 14px;
+    margin-top: 20px;
+    color: ${({ theme }) => theme.colors.lightGrayText};
+    font-weight: bold;
+  }
+`;
