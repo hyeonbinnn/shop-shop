c5f46aa5d92cd2fdcbf28517bcc25dadc6f323f4
hyeonbin jeong <duafuuu@gmail.com>
2023-11-05 02:51:40 +0900

    add: 상품 등록 페이지 주의사항 구현

--- src/pages/ProductUpload/ProductUpload.jsx
+++ src/pages/ProductUpload/ProductUpload.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import * as S from './ProductUpload.style';
+import SellerHeader from './../../components/common/Header/SellerHeader';
 
 const ProductUpload = () => {
-  return <div>ProductUpload</div>;
+  return (
+    <>
+      <SellerHeader />
+      <S.Main>
+        <S.Container>
+          <S.Title>상품 등록</S.Title>
+          <S.Section>
+            <S.TextBox>
+              <p>* 상품 등록 주의사항</p>
+              <div>
+                <ul>
+                  <li>상품명은 공백 포함 20자까지 입력 가능합니다.</li>
+                  <li>상품 이미지 등록 시, 이미지 파일 확장자만 업로드 가능합니다.</li>
+                  <li>판매가와 재고는 0으로 설정할 수 없습니다.</li>
+                  <li>기본 배송비를 0원으로 설정 시, 무료배송으로 등록됩니다.</li>
+                  <li>
+                    각 필드는 필수 입력 정보입니다. 모든 상품 정보가 입력되어야 정상적으로
+                    등록됩니다.
+                  </li>
+                  <li>마켓과 상관없는 상품 정보를 입력할 시, 삭제 될 수 있습니다.</li>
+                </ul>
+              </div>
+            </S.TextBox>
+            <S.ContentBox>
+              <S.ProductInfo>
+                <img />
+                <div></div>
+              </S.ProductInfo>
+              <S.ProductDetail></S.ProductDetail>
+            </S.ContentBox>
+          </S.Section>
+        </S.Container>
+      </S.Main>
+    </>
+  );
 };
 
 export default ProductUpload;
---
+++ src/pages/ProductUpload/ProductUpload.style.jsx
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  padding: 40px 50px;
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    padding: 30px;
+  }
+`;
+
+export const Container = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 0px 20px;
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    padding: 0px 10px;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 30px;
+  margin-bottom: 60px;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  gap: 30px;
+`;
+
+export const TextBox = styled.div`
+  width: 30%;
+
+  p {
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 15px;
+    color: ${({ theme }) => theme.colors.error};
+  }
+
+  div {
+    background-color: ${({ theme }) => theme.colors.notice};
+    border-radius: 5px;
+    padding: 20px;
+
+    ul {
+      li {
+        font-size: 14px;
+        font-weight: bold;
+        margin-bottom: 15px;
+        color: ${({ theme }) => theme.colors.text};
+
+        &::before {
+          content: '-';
+          margin-right: 10px;
+        }
+
+        :last-child {
+          margin-bottom: 0px;
+        }
+      }
+    }
+  }
+`;
+
+export const ContentBox = styled.div`
+  width: 70%;
+  background-color: lightBlue;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+export const ProductInfo = styled.div``;
+
+export const ProductDetail = styled.div``;
