0a26875fd08df4dd9a95009c40fd4dc4979514fa
hyeonbin jeong <duafuuu@gmail.com>
2023-11-04 21:04:04 +0900

    fix: 잘못된 props 전달 및 판매자 상품 관련 함수 로직 수정

--- src/api/seller.js
+++ src/api/seller.js
@@ -1,23 +1,40 @@
 import { axiosInstance } from '../axios';
 
 // 판매자 상품 불러오기
-export const getSellerProduct = async () => {
+export const getSellerProduct = async (token) => {
   try {
-    const res = await axiosInstance.get('seller/');
+    const res = await axiosInstance.get('seller/', {
+      headers: {
+        Authorization: token,
+      },
+    });
     return res.data.results;
   } catch (error) {
-    return error.response;
+    return error.response.data;
   }
 };
 
 // 상품 등록하기
 export const postCreateProduct = async (token, data) => {
   try {
-    const res = await axiosInstance.post('products/', data, {
-      headers: {
-        Authorization: token,
+    const res = await axiosInstance.post(
+      'products/',
+      {
+        product_name: data.product_name,
+        image: data.image,
+        price: data.price,
+        shipping_method: data.shipping_method,
+        shipping_fee: data.shipping_fee,
+        stock: data.stock,
+        product_info: data.product_info,
       },
-    });
+      {
+        headers: {
+          Authorization: token,
+          'Content-Type': 'multipart/form-data',
+        },
+      }
+    );
     alert('상품이 등록되었습니다.');
     return res;
   } catch (error) {
