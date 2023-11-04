727c7a406fea379c76c082ae19a638e27ffc6ad9
hyeonbin jeong <duafuuu@gmail.com>
2023-11-04 21:05:13 +0900

    add: 판매자 상품 리스트 불러오기

--- src/pages/SellerCenter/SellerCenter.jsx
+++ src/pages/SellerCenter/SellerCenter.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { getCookie } from '../../services/cookies';
+import { getSellerProduct } from '../../api/seller';
 import * as S from './SellerCenter.style';
 import SellerHeader from '../../components/common/Header/SellerHeader';
+import SellerItem from './../../components/SellerItem/SellerItem';
 
 const SellerCenter = () => {
+  const token = getCookie('token');
+  const [sellerProductList, setSellerProductList] = useState([]);
+
+  useEffect(() => {
+    getSellerProduct(token).then((res) => {
+      setSellerProductList(res);
+    });
+  }, [token]);
+
   return (
     <>
       <SellerHeader />
@@ -11,7 +23,7 @@ const SellerCenter = () => {
         <S.Container>
           <S.TitleBox>
             <S.Title>
-              대시보드<strong></strong>
+              대시보드 <strong>{sellerProductList[0]?.store_name}</strong>
             </S.Title>
             <Link to="/productUpload">
               <button>상품 업로드</button>
@@ -20,7 +32,7 @@ const SellerCenter = () => {
 
           <S.ContentBox>
             <S.Menu>
-              <li>판매중인 상품</li>
+              <li>판매중인 상품 ({sellerProductList.length})</li>
               <li>주문/배송</li>
               <li>문의/리뷰</li>
               <li>통계</li>
@@ -33,6 +45,11 @@ const SellerCenter = () => {
                 <li>수정</li>
                 <li>삭제</li>
               </S.ItemHeader>
+              {sellerProductList.length > 0
+                ? sellerProductList.map((item) => (
+                    <SellerItem key={item.product_id} token={token} {...item} />
+                  ))
+                : null}
             </S.ProductBox>
           </S.ContentBox>
         </S.Container>
--- src/pages/SellerCenter/SellerCenter.style.jsx
+++ src/pages/SellerCenter/SellerCenter.style.jsx
@@ -12,7 +12,7 @@ export const Main = styled.main`
 export const Container = styled.div`
   max-width: 1280px;
   margin: 0 auto;
-  padding: 0px 20px;
+  /* padding: 0px 20px; */
 
   @media ${(props) => props.theme.mediaQuery.mobile} {
     padding: 0px;
@@ -49,6 +49,10 @@ export const TitleBox = styled.div`
 
 export const Title = styled.h1`
   font-size: 30px;
+
+  strong {
+    color: ${({ theme }) => theme.colors.secondary};
+  }
 `;
 
 export const ContentBox = styled.div`
@@ -60,9 +64,9 @@ export const ContentBox = styled.div`
 export const Menu = styled.ul`
   display: flex;
   flex-direction: column;
+  width: 20%;
 
   li {
-    width: 230px;
     text-align: left;
     font-size: 16px;
     font-weight: bold;
@@ -84,7 +88,7 @@ export const Menu = styled.ul`
 `;
 
 export const ProductBox = styled.div`
-  width: 1000px;
+  width: 80%;
   border: ${({ theme }) => `1px solid ${theme.colors.gray}`};
   background-color: ${({ theme }) => theme.colors.lightGray};
   border-radius: 5px;
@@ -96,10 +100,30 @@ export const ItemHeader = styled.ul`
   border-radius: 5px 5px 0px 0px;
   border-bottom: ${({ theme }) => `1px solid ${theme.colors.gray}`};
   background-color: ${({ theme }) => theme.colors.white};
-  font-size: 16px;
-  font-weight: bold;
 
   li {
+    font-size: 17px;
+    font-weight: bold;
     padding: 15px 10px;
+  }
+
+  li:first-child {
+    margin-left: 120px;
+    margin-right: -120px;
+  }
+
+  li:nth-child(2) {
+    margin-left: 250px;
+    margin-right: -250px;
+  }
+
+  li:nth-child(3) {
+    margin-left: 180px;
+    margin-right: -180px;
+  }
+
+  li:last-child {
+    margin-left: 40px;
+    margin-right: -40px;
   }
 `;
