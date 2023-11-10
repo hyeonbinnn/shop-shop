05a39abd706223634207eb9e6ee3d33e50cf9128
hyeonbin jeong <duafuuu@gmail.com>
2023-11-11 03:51:53 +0900

    add: 상품 정보 등록 폼 구현

--- src/pages/ProductUpload/ProductUpload.jsx
+++ src/pages/ProductUpload/ProductUpload.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { getCookie } from '../../services/cookies';
+import { postCreateProduct, putEditProduct } from '../../api/seller';
 import * as S from './ProductUpload.style';
 import SellerHeader from './../../components/common/Header/SellerHeader';
 
 const ProductUpload = () => {
+  const [inputText, setInputText] = useState('');
+  const [previewImg, setPreviewImg] = useState(null);
+
   return (
     <>
       <SellerHeader />
@@ -11,7 +18,7 @@ const ProductUpload = () => {
           <S.Title>상품 등록</S.Title>
           <S.Section>
             <S.TextBox>
-              <p>* 상품 등록 주의사항</p>
+              <span>* 상품 등록 주의사항</span>
               <div>
                 <ul>
                   <li>상품명은 공백 포함 20자까지 입력 가능합니다.</li>
@@ -27,11 +34,55 @@ const ProductUpload = () => {
               </div>
             </S.TextBox>
             <S.ContentBox>
-              <S.ProductInfo>
-                <img />
-                <div></div>
-              </S.ProductInfo>
-              <S.ProductDetail></S.ProductDetail>
+              <S.TopSection>
+                <S.Image>
+                  <label htmlFor="iamge">상품 이미지</label>
+                  <S.ImageInputBox previewImg={previewImg}>
+                    <input type="file" accept=".jpg, .jpeg, .png, .svg" id="image" />
+                    <button type="button" />
+                  </S.ImageInputBox>
+                </S.Image>
+
+                <S.Info>
+                  <label htmlFor="product_name">상품명</label>
+                  <S.NameInputBox>
+                    <input type="text" id="product_name" maxLength="20" />
+                    <span>{inputText.length} / 20</span>
+                  </S.NameInputBox>
+
+                  <label htmlFor="">판매가</label>
+                  <S.InputBox>
+                    <input type="text" />
+                    <span>원</span>
+                  </S.InputBox>
+
+                  <S.ShippingSpan>배송방법</S.ShippingSpan>
+                  <S.ShippingBox>
+                    <button>택배,소포,등기</button>
+                    <button>직접배송(화물배달)</button>
+                  </S.ShippingBox>
+
+                  <label htmlFor="">기본 배송비</label>
+                  <S.InputBox>
+                    <input type="text" />
+                    <span>원</span>
+                  </S.InputBox>
+
+                  <label htmlFor="">재고</label>
+                  <S.InputBox>
+                    <input type="text" />
+                    <span>개</span>
+                  </S.InputBox>
+                </S.Info>
+              </S.TopSection>
+              <S.BottomSection>
+                <label htmlFor="product_info">상품 상세 정보</label>
+                <input type="text" id='"product_info"' />
+              </S.BottomSection>
+              <S.ButtonBox>
+                <button>취소</button>
+                <button>저장하기</button>
+              </S.ButtonBox>
             </S.ContentBox>
           </S.Section>
         </S.Container>
--- src/pages/ProductUpload/ProductUpload.style.jsx
+++ src/pages/ProductUpload/ProductUpload.style.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import preview from '../../assets/icons/icon-preview.png';
 
 export const Main = styled.main`
   padding: 40px 50px;
@@ -25,16 +26,17 @@ export const Title = styled.h1`
 
 export const Section = styled.section`
   display: flex;
-  gap: 30px;
+  gap: 80px;
 `;
 
 export const TextBox = styled.div`
   width: 30%;
 
-  p {
+  span {
+    display: block;
     font-size: 16px;
     font-weight: bold;
-    margin-bottom: 15px;
+    margin-bottom: 10px;
     color: ${({ theme }) => theme.colors.error};
   }
 
@@ -63,14 +65,160 @@ export const TextBox = styled.div`
   }
 `;
 
-export const ContentBox = styled.div`
+export const ContentBox = styled.form`
   width: 70%;
-  background-color: lightBlue;
   display: flex;
   flex-direction: column;
+  margin-bottom: 70px;
+  gap: 40px;
+
+  label {
+    display: block;
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 10px;
+    color: ${({ theme }) => theme.colors.lightGrayText};
+  }
+
+  input {
+    :focus {
+      outline: none;
+    }
+  }
+`;
+
+export const TopSection = styled.section`
+  display: flex;
+  justify-content: space-between;
   gap: 40px;
 `;
 
-export const ProductInfo = styled.div``;
+export const Image = styled.div``;
 
-export const ProductDetail = styled.div``;
+export const ImageInputBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 410px;
+  height: 410px;
+  background: ${({ previewImg, theme }) =>
+    previewImg ? `url(${preview}) no-repeat center / cover` : theme.colors.gray};
+  border-radius: 5px;
+`;
+
+export const Info = styled.div`
+  div {
+    margin-bottom: 10px;
+
+    :last-child {
+      margin-bottom: 0px;
+    }
+  }
+`;
+
+export const InputBox = styled.div`
+  display: flex;
+  width: 200px;
+  height: 50px;
+  border: ${({ theme }) => `2px solid ${theme.colors.gray}`};
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 5px;
+  font-size: 14px;
+
+  input {
+    width: 100%;
+    border-radius: 5px 0 0 5px;
+    padding: 0px 10px;
+  }
+
+  span {
+    width: 50px;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${({ theme }) => theme.colors.gray};
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+export const NameInputBox = styled(InputBox)`
+  min-width: 430px;
+  display: flex;
+  justify-content: space-between;
+
+  input {
+    width: 100%;
+  }
+
+  span {
+    width: 70px;
+    background-color: transparent;
+    padding: 0 5px;
+    color: ${({ theme }) => theme.colors.gray};
+  }
+`;
+
+export const ShippingSpan = styled.span`
+  display: block;
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.colors.lightGrayText};
+`;
+
+export const ShippingBox = styled.div`
+  display: flex;
+
+  button {
+    width: 200px;
+    height: 50px;
+    padding: 10px;
+    font-weight: bold;
+    border-radius: 5px;
+
+    :first-child {
+      margin-right: 10px;
+      background-color: ${({ theme }) => theme.colors.secondary};
+      color: ${({ theme }) => theme.colors.white};
+      border: ${({ theme }) => `2px solid ${theme.colors.secondary}`};
+    }
+
+    :last-child {
+      background-color: ${({ theme }) => theme.colors.white};
+      color: ${({ theme }) => theme.colors.lightGrayText};
+      border: ${({ theme }) => `2px solid ${theme.colors.gray}`};
+    }
+  }
+`;
+
+export const BottomSection = styled.section`
+  input {
+    width: 100%;
+    height: 700px;
+    background-color: ${({ theme }) => theme.colors.lightGray};
+    border-radius: 5px;
+    border: ${({ theme }) => `2px solid ${theme.colors.gray}`};
+    padding: 20px;
+  }
+`;
+
+export const ButtonBox = styled(ShippingBox)`
+  justify-content: flex-end;
+
+  button {
+    font-size: 16px;
+
+    :first-child {
+      background-color: ${({ theme }) => theme.colors.white};
+      color: ${({ theme }) => theme.colors.lightGrayText};
+      border: ${({ theme }) => `2px solid ${theme.colors.gray}`};
+    }
+
+    :last-child {
+      background-color: ${({ theme }) => theme.colors.secondary};
+      color: ${({ theme }) => theme.colors.white};
+      border: ${({ theme }) => `2px solid ${theme.colors.secondary}`};
+    }
+  }
+`;
