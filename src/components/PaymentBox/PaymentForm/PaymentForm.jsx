934c82942ad50bc0f40322d2081fc60d437405cf
hyeonbin jeong <duafuuu@gmail.com>
2023-11-27 22:02:34 +0900

    add: 배송정보, 결제수단, 최종결제 정보 구현

--- src/components/PaymentBox/PaymentForm/PaymentForm.jsx
+++ src/components/PaymentBox/PaymentForm/PaymentForm.jsx
@@ -1,13 +1,118 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as S from './PaymentForm.style';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import DaumPostCoed from 'react-daum-postcode';
+import { getCookie } from '../../../services/cookies';
+import { postCreateOrder } from '../../../api/order';
 
-const PaymentForm = () => {
+const PaymentForm = ({ products, totalPrice, totalFee, totalPay }) => {
+  const navigate = useNavigate();
+  const token = getCookie('token');
+
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    watch,
+  } = useForm({ mode: 'onBlur', defaultValues: { checkBox: false } });
+
+  const checkValid = watch('checkBox');
+
+  const onSubmit = handleSubmit((data) => {});
+
   return (
-    <>
-      <form>
-        <label></label>
-        <input></input>
-      </form>
-    </>
+    <S.Form>
+      <S.ShippingInfo>
+        <S.Title>배송정보</S.Title>
+        <S.SubTitle>주문자 정보</S.SubTitle>
+        <S.InfoBox>
+          <label htmlFor="name">이름</label>
+          <input />
+        </S.InfoBox>
+        <S.InfoBox>
+          <label htmlFor="phoneNum">휴대폰</label>
+          <input />
+          <span>-</span>
+          <input />
+          <span>-</span>
+          <input />
+        </S.InfoBox>
+        <S.InfoBox>
+          <label htmlFor="email">이메일</label>
+          <input />
+        </S.InfoBox>
+
+        <S.SubTitle>배송지 정보</S.SubTitle>
+        <S.InfoBox>
+          <label htmlFor="receiver">수령인</label>
+          <input />
+        </S.InfoBox>
+        <S.InfoBox>
+          <label htmlFor="phoneNum">휴대폰</label>
+          <input />
+          <span>-</span>
+          <input />
+          <span>-</span>
+          <input />
+        </S.InfoBox>
+        <S.InfoBox>
+          <label htmlFor="address">배송주소</label>
+          <input />
+        </S.InfoBox>
+        <S.InfoBox>
+          <label htmlFor="address_message">배송메시지</label>
+          <input />
+        </S.InfoBox>
+      </S.ShippingInfo>
+
+      <S.PaymentInfo>
+        <S.PaymentMethod>
+          <S.Title>결제수단</S.Title>
+          <S.MethodBox>
+            <input />
+            <label htmlFor="card">신용/체크카드</label>
+            <input />
+            <label htmlFor="deposit">무통장 입금</label>
+            <input />
+            <label htmlFor="phonePay">휴대폰 결제</label>
+            <input />
+            <label htmlFor="naverPay">네이버페이</label>
+            <input />
+            <label htmlFor="kakaoPay">카카오페이</label>
+          </S.MethodBox>
+        </S.PaymentMethod>
+
+        <S.FinalPayment>
+          <S.FinalTitle>최종결제 정보</S.FinalTitle>
+          <S.FinalBox>
+            <S.CheckFinal>
+              <S.TopInfo>
+                <span>- 상품금액</span>
+                <strong></strong>
+              </S.TopInfo>
+              <S.TopInfo>
+                <span>- 할인금액</span>
+                <strong></strong>
+              </S.TopInfo>
+              <S.TopInfo>
+                <span>- 배송비</span>
+                <strong></strong>
+              </S.TopInfo>
+              <S.BottomInfo>
+                <span>- 결제금액</span>
+                <strong></strong>
+              </S.BottomInfo>
+            </S.CheckFinal>
+            <S.GoToFinal>
+              <input />
+              <label htmlFor="check">주문 내용을 확인하였으며, 정보 제공 등에 동의합니다.</label>
+              <button>결제하기</button>
+            </S.GoToFinal>
+          </S.FinalBox>
+        </S.FinalPayment>
+      </S.PaymentInfo>
+    </S.Form>
   );
 };
 
--- src/components/PaymentBox/PaymentForm/PaymentForm.style.jsx
+++ src/components/PaymentBox/PaymentForm/PaymentForm.style.jsx
@@ -1,1 +1,149 @@
 import styled from 'styled-components';
+
+export const Form = styled.form`
+  margin: 90px 0px;
+  display: flex;
+  flex-direction: column;
+  gap: 70px;
+  text-align: left;
+
+  label,
+  span {
+    font-size: 16px;
+    font-weight: bold;
+  }
+`;
+
+export const Title = styled.h2`
+  padding-bottom: 18px;
+  border-bottom: ${({ theme }) => `2px solid ${theme.colors.gray}`};
+  font-size: 23px;
+
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+export const SubTitle = styled.h3`
+  padding: 40px 0px 10px 0px;
+  border-bottom: ${({ theme }) => `2px solid ${theme.colors.gray}`};
+  font-size: 18px;
+`;
+
+export const FinalTitle = styled(Title)`
+  border-bottom: none;
+`;
+
+export const ShippingInfo = styled.section``;
+
+export const InfoBox = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px 0px;
+  border-bottom: ${({ theme }) => `1px solid ${theme.colors.third}`};
+
+  label {
+    width: 200px;
+  }
+
+  input {
+    height: 40px;
+    border: ${({ theme }) => `1px solid ${theme.colors.third}`};
+    outline: none;
+    padding-left: 10px;
+  }
+
+  span {
+    margin: 0px 10px;
+    color: ${({ theme }) => theme.colors.lightGrayText};
+  }
+`;
+
+export const PaymentInfo = styled.section`
+  display: flex;
+  gap: 40px;
+
+  @media ${(props) => props.theme.mediaQuery.mobile} {
+    gap: 70px;
+    flex-direction: column;
+  }
+`;
+
+export const PaymentMethod = styled.div``;
+
+export const MethodBox = styled.div`
+  border-bottom: ${({ theme }) => `2px solid ${theme.colors.gray}`};
+  padding: 18px 12px 18px 12px;
+  line-height: 2;
+
+  input {
+    width: 20px;
+    height: 20px;
+    flex-shrink: 0;
+    border: ${({ theme }) => `2px solid ${theme.colors.gray}`};
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+
+  label {
+    margin-right: 20px;
+
+    &:last-child {
+      margin-right: 0px;
+    }
+  }
+`;
+
+export const FinalPayment = styled.div``;
+
+export const FinalBox = styled.div`
+  border-radius: 10px;
+  border: ${({ theme }) => `2px solid ${theme.colors.secondary}`};
+  overflow: hidden;
+`;
+
+export const CheckFinal = styled.div`
+  padding: 34px 30px 25px 30px;
+`;
+
+export const TopInfo = styled.div`
+  margin-bottom: 15px;
+
+  :nth-child(3) {
+    padding-bottom: 20px;
+    border-bottom: ${({ theme }) => `2px solid ${theme.colors.gray}`};
+  }
+`;
+
+export const BottomInfo = styled.div`
+  margin-top: 29px;
+`;
+
+export const GoToFinal = styled.div`
+  padding: 30px;
+  background-color: ${({ theme }) => theme.colors.lightGray};
+
+  input {
+    width: 20px;
+    height: 20px;
+    flex-shrink: 0;
+    border: ${({ theme }) => `2px solid ${theme.colors.gray}`};
+    margin-right: 10px;
+  }
+
+  label {
+    line-height: 1.5;
+  }
+
+  button {
+    display: block;
+    width: 220px;
+    color: ${({ theme }) => theme.colors.white};
+    background-color: ${({ theme }) => theme.colors.secondary};
+    margin: 30px auto 0px;
+    font-size: 23px;
+    font-weight: bold;
+    padding: 19px 60px;
+    border-radius: 10px;
+  }
+`;
